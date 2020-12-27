2b0db5cbceec9851831a18ccdb1fe540019944d7
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2020-12-27 01:40:43 -0700

    chore: update prop types

--- src/library.js
+++ src/library.js
@@ -14,7 +14,19 @@ class Library {
 }
 
 Library.propTypes = {
-  corganizeClient: PropTypes.shape(CorganizeClient).isRequired,
+  config: PropTypes.shape({
+    server: PropTypes.shape({
+      host: PropTypes.string,
+      adpikey: PropTypes.string,
+    }),
+    storageservice: PropTypes.shape({
+      gdrive: PropTypes.shape({
+        creds: PropTypes.shape({
+          path: PropTypes.string,
+        }),
+      }),
+    }),
+  }).isRequired,
 };
 
 export default Library;
