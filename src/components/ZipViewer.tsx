4d7f5c18088ef956c8d0c9ec1b481325fd39cff7
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2021-01-18 18:35:44 -0700

    feat: use react-images for the lightbox

--- src/components/ZipViewer.tsx
+++ src/components/ZipViewer.tsx
@@ -1,6 +1,7 @@
 import { readdirSync } from 'fs';
 import React, { useEffect, useState } from 'react';
-import Gallery from 'react-grid-gallery';
+import Carousel, { Modal, ModalGateway } from 'react-images';
+import Button from './Button';
 
 import './ZipViewer.scss';
 
@@ -11,6 +12,7 @@ const AdmZip = require('adm-zip');
 const ZipViewer = ({ path }) => {
   const [images, setImages] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [modalIsOpen, setModalIsOpen] = useState(true);
 
   useEffect(() => {
     const dir = `${path}.deflated`;
@@ -26,15 +28,9 @@ const ZipViewer = ({ path }) => {
     const imgs = readdirSync(dir).map((filename) => {
       const fullPath = join(dir, filename);
       return {
-        src: `file://${fullPath}`,
-        thumbnail: `file://${fullPath}`,
-        thumbnailWidth: 0,
-        thumbnailHeight: 0,
-        caption: filename,
+        source: `file://${fullPath}`,
       };
     });
-
-    imgs[0].isSelected = true;
 
     setImages(imgs);
   }, [path]);
@@ -49,7 +45,14 @@ const ZipViewer = ({ path }) => {
 
   return (
     <div className="zip-viewer">
-      <Gallery images={images} isOpen />
+      <ModalGateway>
+        {modalIsOpen && (
+          <Modal onClose={() => setModalIsOpen(!modalIsOpen)}>
+            <Carousel views={images} />
+          </Modal>
+        )}
+      </ModalGateway>
+      <Button onClick={() => setModalIsOpen(true)}>Open Lightbox</Button>
     </div>
   );
 };
