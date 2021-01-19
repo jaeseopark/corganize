de8584238fd02200cc1dbdb1d78422ef55ed3548
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2021-01-18 17:30:53 -0700

    feat: zip viewer

--- src/components/ZipViewer.tsx
+++ src/components/ZipViewer.tsx
@@ -1,14 +1,57 @@
-import React from 'react';
+import { readdirSync } from 'fs';
+import React, { useEffect, useState } from 'react';
+import Gallery from 'react-grid-gallery';
+
+import './ZipViewer.scss';
+
+const { join } = require('path');
 
 const AdmZip = require('adm-zip');
 
 const ZipViewer = ({ path }) => {
-  const zip = new AdmZip(path);
-  const defaltedPath = `${path}.deflated`;
+  const [images, setImages] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
-  zip.extractAllTo(defaltedPath);
+  useEffect(() => {
+    const dir = `${path}.deflated`;
 
-  return <h1>hello</h1>;
+    try {
+      const zip = new AdmZip(path);
+      zip.extractAllTo(dir);
+    } catch (e) {
+      setErrorMessage(e.message);
+      return;
+    }
+
+    const imgs = readdirSync(dir).map((filename) => {
+      const fullPath = join(dir, filename);
+      return {
+        src: `file://${fullPath}`,
+        thumbnail: `file://${fullPath}`,
+        thumbnailWidth: 0,
+        thumbnailHeight: 0,
+        caption: filename,
+      };
+    });
+
+    imgs[0].isSelected = true;
+
+    setImages(imgs);
+  }, [path]);
+
+  if (errorMessage) {
+    return <p>{errorMessage}</p>;
+  }
+
+  if (!images) {
+    return null;
+  }
+
+  return (
+    <div className="zip-viewer">
+      <Gallery images={images} isOpen />
+    </div>
+  );
 };
 
 export default ZipViewer;
