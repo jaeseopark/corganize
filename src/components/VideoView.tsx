04676362bc2e907a8d7dbc756a88d7198c9dd111
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2021-03-06 11:11:49 -0700

    feat: add hotkeys for video (#72)

--- src/components/VideoView.tsx
+++ src/components/VideoView.tsx
@@ -2,14 +2,29 @@ import React, { useRef } from 'react';
 import screenfull from 'screenfull';
 import './VideoView.scss';
 
+const TIME_HOTKEY_MAP = {
+  z: -60,
+  x: -15,
+  c: 15,
+  v: 60,
+};
+
+const isHotkey = (key: string) => Object.keys(TIME_HOTKEY_MAP).includes(key);
+
 const VideoView = ({ path }) => {
   const el = useRef(null);
+
+  const jumpTime = (deltaInSeconds) => {
+    el.current.currentTime += deltaInSeconds;
+  };
 
   const onKeyUp = (event) => {
     if (event.key === 'f') {
       if (screenfull.isEnabled) {
         screenfull.toggle(el.current);
       }
+    } else if (isHotkey(event.key)) {
+      jumpTime(TIME_HOTKEY_MAP[event.key]);
     }
   };
 
