71c434a7d7f8572ab4cc10f1d5fad72ef8187d22
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2021-03-09 01:37:48 -0700

    feat: display the first decimal place for GB and larger (#75)

--- src/utils/numberUtils.ts
+++ src/utils/numberUtils.ts
@@ -1,8 +1,9 @@
-/* eslint-disable import/prefer-default-export */
+const UNITS = ['B', 'kB', 'MB', 'GB', 'TB', 'PB'];
+
+// eslint-disable-next-line import/prefer-default-export
 export function humanFileSize(sizeInBytes: number) {
   const i = Math.floor(Math.log(sizeInBytes) / Math.log(1024));
   // eslint-disable-next-line no-restricted-properties
-  const value = (sizeInBytes / Math.pow(1024, i)).toFixed(0);
-  const unit = ['B', 'kB', 'MB', 'GB', 'TB'][i];
-  return `${value} ${unit}`;
+  const value = (sizeInBytes / Math.pow(1024, i)).toFixed(i < 3 ? 0 : 1);
+  return `${value} ${UNITS[i]}`;
 }
