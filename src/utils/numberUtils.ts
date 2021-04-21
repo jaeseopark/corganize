ab1db497ffdb61b122b5971c58ad6c51573e9714
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2021-04-21 02:05:09 -0600

    fix: handle leading zeros

--- src/utils/numberUtils.ts
+++ src/utils/numberUtils.ts
@@ -20,5 +20,5 @@ export const toHumanDuration = (seconds: number): string =>
   new Date(seconds * 1000)
     .toISOString()
     .substr(seconds < 3600 ? 14 : 11, seconds < 3600 ? 5 : 8)
-    .replace(/^0+/, '')
+    .substr(seconds < 600 ? 1 : 0)
     .replaceAll('-', ':');
