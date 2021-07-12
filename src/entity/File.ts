354c72f14e022ed914675a3ebf283edb72a6a0f1
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2021-07-11 21:30:32 -0600

    chore: inline comment

--- src/entity/File.ts
+++ src/entity/File.ts
@@ -1,11 +1,11 @@
 export type File = {
-  // Mandatory fields
+  // Mandatory fields (server-side)
   fileid: string;
   sourceurl: string;
   lastupdated: number;
   filename: string;
 
-  // Optional fields (still server-side)
+  // Optional fields (server-side)
   dateactivated?: number;
   storageservice?: string;
   locationref?: string;
