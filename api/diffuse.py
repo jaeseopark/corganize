3cff252282702229ce14ddb47008373f4705de33
jaeseopark <20038316+jaeseopark@users.noreply.github.com>
2024-11-19 00:39:39 -0700

    feat: global request template

--- api/diffuse.py
+++ api/diffuse.py
@@ -2,14 +2,16 @@ import re
 from typing import Any, List
 
 from utils import get_shuffled_copy
+
 MAX_PRESET_LEN = 30
+KEY_BLACKLIST = set(["type", "notes", "weight"])
 
 
 def randomize(s: Any):
     if isinstance(s, str):
-        matches = re.findall(r"(\([^()]+\))", s)
+        matches = re.findall(r"(\[[^()]+\])", s)
         for match in matches:
-            candidates = match.strip("(").strip(")").split("|")
+            candidates = match.strip("[").strip("]").split("|")
             s = s.replace(match, get_shuffled_copy(candidates)[0])
     return s
 
@@ -28,21 +30,24 @@ class DiffuseRequest:
     def to_diffbee_payload(self, num_imgs=1) -> dict:
         # See https://github.com/jaeseopark/diffusionbee-stable-diffusion-rest-api?tab=readme-ov-file#payload
         return {
-            **{k: randomize(v) for k, v in self._request.items()},
+            **{k: randomize(v) for k, v in self._request.items() if k not in KEY_BLACKLIST},
             "selected_aspect_ratio": "Portrait",
             "img_width": 448,
             "img_height": 576,
-            "guidance_scale": 6.5,
             "num_imgs": num_imgs
         }
 
 
 def _expand(requests: List[dict]):
+    global_request, * \
+        _ = [r for r in requests if r.get("type") == "global"] or [{}]
+
     expanded: List[DiffuseRequest] = []
     for request in requests:
-        if not request.get("enabled", True):
+        if request.get("type") == "global" or not request.get("enabled", True):
             continue
-        expanded.extend([DiffuseRequest(request)] * request.get("weight", 1))
+        expanded.extend(
+            [DiffuseRequest({**global_request, **request})] * request.get("weight", 1))
     return expanded
 
 
