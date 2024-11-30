59870a365a95d14cb3fab3b645e9d900f5a3573f
jaeseopark <20038316+jaeseopark@users.noreply.github.com>
2024-11-30 01:24:21 -0700

    refactor: introduce prompt concatenator

--- api/diffuse/preset.py
+++ api/diffuse/preset.py
@@ -1,8 +1,8 @@
-from random import choices, randint, uniform
+from random import randint
 import re
-from typing import Callable, List
 import json
 
+from diffuse.prompt_concatenator import PromptConcatenator
 from diffuse.template_consumer import TemplateConsumer
 from diffuse.randomizer import Randomizer
 
@@ -27,47 +27,6 @@ def get_resolve_func(prompt_lookup: dict):
     return _resolve
 
 
-def _randomize_weights(loras: List[dict]):
-    if not loras:
-        return []
-
-    def randomize_lora(loras: list):
-        ret_loras = []
-        for lora in loras:
-            if "one_of" in lora:
-                candidates: List[dict] = lora["one_of"]
-                lora = choices(candidates, k=1)
-            ret_loras.append(lora)
-        return ret_loras
-
-    for lora in randomize_lora(loras):
-        weight = lora.get("weight")
-        if not isinstance(weight, list):
-            continue
-
-        assert len(weight) == 2, "a weight range must be 2"
-        picked_weight = uniform(weight[0], weight[1])
-        lora["weight"] = picked_weight
-
-
-def _get_lora_tags(loras: List[dict]) -> str:
-    loras = loras or []
-
-    aliases = [lora["alias"] for lora in loras]
-    msg = "duplicate loras should not exist"
-    assert len(aliases) == len(set(aliases)), msg
-
-    def get_tag(lora: dict):
-        return f"<lora:{lora['alias']}:{lora['weight']}>"
-    return "".join(map(get_tag, loras))
-
-
-def _concatenate(prompt_elements: List[str], prompt: str, loras: List[str], resolve: Callable) -> str:
-    kws = [resolve(kw) for kw in prompt_elements or []]
-    prompt = ",".join([kw for kw in kws if kw] + [prompt])
-    return resolve(prompt) + _get_lora_tags(loras)
-
-
 def _get_req_body(preset: dict, conf: dict) -> dict:
     """
     See https://github.com/AUTOMATIC1111/stable-diffusion-webui/wiki/API
@@ -75,15 +34,15 @@ def _get_req_body(preset: dict, conf: dict) -> dict:
     allowed_keys = conf.get("allowed_keys")
     resolve = get_resolve_func(conf.get("saved_prompts"))
     template_consumer = TemplateConsumer(conf.get("templates"), resolve)
+    prompt_concatenator = PromptConcatenator(resolve)
     preset = template_consumer.consume(preset)
 
     assert preset.get("model"), "'model' must be set"
 
-    preset["prompt"] = _concatenate(
+    preset["prompt"] = prompt_concatenator.concatenate(
         preset.get("prompt_elements"),
         preset.get("prompt"),
-        _randomize_weights(preset.get("loras")),
-        resolve
+        preset.get("loras")
     )
 
     return {
@@ -111,6 +70,9 @@ class DiffusePreset:
         templates = preset.get("templates", [])
         assert isinstance(templates, list), "'templates' must be a list"
         preset["templates"] = templates + ["default"]
+
+        msg = "'loras' must be a list"
+        assert isinstance(preset.get("loras", []), list), msg
 
         self._specs = preset
         self._conf = conf
---
+++ api/diffuse/prompt_concatenator.py
@@ -0,0 +1,62 @@
+import json
+from typing import Callable, List, Union
+from random import choices, uniform
+from typing import List
+
+
+def _randomize_lora(loras: List[dict]) -> List[dict]:
+    ret_loras = []
+    for lora in loras:
+        if "one_of" in lora:
+            candidates: List[dict] = lora["one_of"]
+            lora = choices(candidates, k=1)
+        ret_loras.append({**lora})
+    return ret_loras
+
+
+def _randomize_weights(loras: List[dict]) -> List[dict]:
+    """
+    Converts weight=[float, float] to a single float.
+    """
+
+    # make a copy
+    loras = json.loads(json.dumps(loras))
+
+    for lora in loras:
+        weight = lora.get("weight")
+        if not isinstance(weight, list):
+            continue
+
+        assert len(weight) == 2, "a weight range must be 2"
+        picked_weight = uniform(weight[0], weight[1])
+        lora["weight"] = picked_weight
+
+    return loras
+
+
+def _get_lora_tags(loras: List[dict]) -> str:
+    loras = _randomize_lora(loras)  # process "one_of"s
+    loras = _randomize_weights(loras)  # process weight ranges
+
+    # A little set of validations
+    for lora in loras:
+        assert "alias" in lora, f"'alias' must be set {lora=}"
+        assert "weight" in lora, f"'weight' must be set {lora=}"
+
+    aliases = [lora["alias"] for lora in loras]
+    msg = "duplicate loras should not exist"
+    assert len(aliases) == len(set(aliases)), msg
+
+    def get_tag(lora: dict):
+        return f"<lora:{lora['alias']}:{lora['weight']}>"
+    return "".join(map(get_tag, loras))
+
+
+class PromptConcatenator:
+    def __init__(self, resolve: Callable) -> None:
+        self.resolve = resolve
+
+    def concatenate(self, prompt_elements: Union[List[str], None], prompt: str, loras: Union[List[dict], None]) -> str:
+        kws = [self.resolve(kw) for kw in prompt_elements or []]
+        prompt = ",".join([kw for kw in kws if kw] + [prompt])
+        return self.resolve(prompt) + _get_lora_tags(loras or [])
